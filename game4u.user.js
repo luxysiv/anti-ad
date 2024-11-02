d3d2a201f0e383cb27a85b21d2104e726ef3a10c
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-11-02 22:11:21 +0700

    Best

--- game4u.user.js
+++ game4u.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Block pop-up game4u
 // @namespace    luxysiv
-// @version      1.1
+// @version      1.2
 // @description  Block pop-up game4u
 // @author       Mạnh Dương
 // @match        https://game4u.mobi/*
@@ -14,13 +14,39 @@
 (function() {
     'use strict';
 
-    // Remove script tags with src="about:blank"
-    document.querySelectorAll('script[src="about:blank"]').forEach(script => {
-        script.remove();
-    });
+    // Block ads by removing elements with known ad IDs or classes
+    function blockAds() {
+        const adsContainers = document.querySelectorAll('#ads, .ad, .advertisement, iframe');
+        adsContainers.forEach(ad => ad.remove());
+    }
 
-    // Remove iframe tags with src="about:blank"
-    document.querySelectorAll('iframe[src="about:blank"]').forEach(iframe => {
-        iframe.remove();
-    });
+    // Block pop-ups by preventing new windows or tabs
+    function blockPopUps() {
+        const openBackup = window.open;
+        window.open = function() {
+            console.log('Blocked a pop-up');
+            return null;
+        };
+    }
+
+    // Block redirects by monitoring URL changes
+    function blockRedirects() {
+        let lastUrl = location.href;
+        new MutationObserver(() => {
+            const url = location.href;
+            if (url !== lastUrl) {
+                console.log('Blocked a redirect to: ' + url);
+                history.pushState(null, '', lastUrl); // Ngăn chuyển hướng bằng cách quay lại URL cũ
+            }
+            lastUrl = url;
+        }).observe(document, { subtree: true, childList: true });
+    }
+
+    // Run the functions
+    blockAds();
+    blockPopUps();
+    blockRedirects();
+
+    // Additional listener for dynamic content loading
+    document.addEventListener('DOMNodeInserted', blockAds);
 })();
