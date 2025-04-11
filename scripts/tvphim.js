2ae783fa5e75c918f2f9268e738783f0132473f6
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2025-04-11 09:40:25 +0700

    Script đúng

--- scripts/tvphim.js
+++ scripts/tvphim.js
@@ -1,18 +1,19 @@
 // Set a cookie with the key 'popupOpeneds' and value 'true' to prevent showing popup ads
 document.cookie = "popupOpeneds=true; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT;";
 
-// Add CSS to hide elements (static and dynamic)
-const style = document.createElement("style");
-style.textContent = `
+// List of selectors to hide
+const selectors = [
   '[src*="/ads/"][src$=".gif"]',
   '[src*="/ads/"][src$=".mp4"]',
   '[id*="floating_ads_"]',
   '[class*="banner-ads"]',
-  '[id*="adsmessage"]' {
-    display: none !important;
-    visibility: hidden !important;
-  }
-`;
+  '[id*="adsmessage"]'
+];
+
+// Create and add CSS to <head> to hide elements
+const style = document.createElement("style");
+style.type = "text/css";
+style.textContent = selectors.map(selector => `${selector} { display: none !important; }`).join("\n");
 document.head.appendChild(style);
 
 // Hàm tua nhanh video quảng cáo
