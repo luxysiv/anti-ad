314f39249fd0672e2de5c6e68bcc35a0746ab633
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2025-04-11 08:17:05 +0700

    Cập nhật motchill

--- scripts/motchill.js
+++ scripts/motchill.js
@@ -1,6 +1,3 @@
-// Set a cookie with the key 'popupOpened' and value 'true' to prevent showing popup ads
-document.cookie = "popupOpened=true; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT;";
-
 // Inject CSS to hide ad elements
 const css = `
     .catfish-img {
